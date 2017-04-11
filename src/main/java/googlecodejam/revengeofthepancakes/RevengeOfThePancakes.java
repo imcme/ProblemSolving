043e4f963e320d04b5ca975d8d686ed25b52bdde
MinCheol Lim <lim.mincheol@nhn.com>
2017-04-11 16:04:43 +0900

    Revenge of the Pancakes - 정답으로 필요한것은 몇회 뒤집었냐지, 과정이 아니다, 그래서 할당하는 부분은 삭제하였다.

--- src/main/java/googlecodejam/revengeofthepancakes/RevengeOfThePancakes.java
+++ src/main/java/googlecodejam/revengeofthepancakes/RevengeOfThePancakes.java
@@ -10,7 +10,6 @@ import java.io.IOException;
  * https://code.google.com/codejam/contest/6254486/dashboard#s=p1&a=1
  *
  * 예시에 함정이 있다. 5번 test case가 꼭 풀이 예시처럼 풀 필요는 없다.
- *
  * 위에서부터 차례로 모양이 달라질때, 뒤집으면 최소가 된다.
  *
  * @author MinCheol Lim <lim.mincheol@nhn.com>
@@ -29,18 +28,12 @@ public class RevengeOfThePancakes {
 			char[] pancakes = S.toCharArray();
 			int count = 0;
 			for (int i = 1; i < pancakes.length; i++) {
-				// 처음 것과 달라지면 그 인덱스전까지 뒤집는다.
-				// 어차피 2지선다이기때문에, 코드를 줄여보려 i번째것을 j번째로 넣는것으로 만들었다.
-				if (pancakes[0] != pancakes[i]) {
-					for (int j = 0; j < i; j++) {
-						pancakes[j] = pancakes[i];
-					}
-
+				if (pancakes[i - 1] != pancakes[i]) {
 					count++;
 				}
 			}
 
-			if (pancakes[0] == '-') {
+			if (pancakes[pancakes.length - 1] == '-') {
 				count++;
 			}
 
